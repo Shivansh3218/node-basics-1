60f0e720fe6cb810b3d01e312ab8d9ad87fe774d
SHIVANSH RAWAT <Shivansh Rawat>
2022-06-17 20:03:47 +0530

    by while loop

---
+++ 2ndrepeat.js
@@ -0,0 +1,10 @@
+let i = parseInt(process.argv[2])
+let n = parseInt(process.argv[3])
+
+let a=i
+while(a<=n){
+    if(a%2==0){
+    console.log(a)
+}
+    a++
+}
---
+++ 3rdrepeat.js
@@ -0,0 +1,10 @@
+let i = parseInt(process.argv[2])
+let n = parseInt(process.argv[3])
+
+let a=i
+while(a>=n){
+  
+    console.log(a)
+
+    a--
+}
